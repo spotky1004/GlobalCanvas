e838f92e884e6c569561d49d9dad95036e462e42
Spotky1004 <spotky1004@gmail.com>
2022-04-04 02:29:25 +0900

    Update db.ts

--- src/db.ts
+++ src/db.ts
@@ -1,4 +1,8 @@
 import { MongoClient } from 'mongodb';
+import dotenv from "dotenv";
+
+dotenv.config();
+
 const uri = `mongodb+srv://spotky1004:${process.env.MONGODB_PASSWORD}@cluster0.rlp2b.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri);
 const dbName = 'GlobalCanvas';

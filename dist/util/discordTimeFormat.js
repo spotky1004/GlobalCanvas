50bd5ff432e3be4e4fda1131efc356b2107f39f0
Spotky1004 <spotky1004@gmail.com>
2022-04-10 15:00:02 +0900

    Delete discordTimeFormat.js

--- dist/util/discordTimeFormat.js
+++
@@ -1,4 +0,0 @@
-export default function discordCooldownFormat(currentTime, leftTime) {
-    const timestamp = Math.floor((currentTime + leftTime) / 1000);
-    return `<t:${timestamp}:f> (<t:${timestamp}:R>)`;
-}

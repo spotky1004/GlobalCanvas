3a37e29200cfa06e0ff3041313641edb508b0f17
Spotky1004 <spotky1004@gmail.com>
2022-04-11 17:32:51 +0900

    Build

--- dist/class/Logger.js
+++ dist/class/Logger.js
@@ -16,26 +16,30 @@ class Logger {
         if (this.loggingIdx === null)
             return;
         for (let i = 0; i < this.logCache.length; i++) {
-            await this.saveLog(this.logCache[i]);
+            const cache = this.logCache[i];
+            await this.saveLog(cache[0], cache[1]);
         }
         await this.collection.updateOne({ _id: "loggingIdx" }, { $set: { idx: this.loggingIdx.toString() } }, { upsert: true });
         this.logCache = [];
     }
-    async saveLog(toLog) {
+    async saveLog(toLog, time) {
         if (this.loggingIdx === null)
             return;
-        const data = { data: toLog };
+        const data = { data: toLog, time };
         await this.collection.updateOne({ _id: this.loggingIdx.toString() }, { $set: data }, { upsert: true });
         this.loggingIdx++;
     }
+    addLog(toLog) {
+        this.logCache.push([toLog, new Date().getTime()]);
+    }
     addFillLog(userId, colorHex, x, y) {
-        this.logCache.push(`[Fill] ${colorHex} (${x}, ${y}) [By ${userId}]`);
+        this.addLog(`[Fill] ${colorHex} (${x}, ${y}) [By ${userId}]`);
     }
     addConnectLog(userId, guildId, channelId) {
-        this.logCache.push(`[Connect] Guild ${guildId} -> Channel ${channelId} [By ${userId}]`);
+        this.addLog(`[Connect] Guild ${guildId} -> Channel ${channelId} [By ${userId}]`);
     }
     addZoomLog(userId, x, y, width, height) {
-        this.logCache.push(`[Zoom] x: ${x}, y: ${y}, width: ${width}, height: ${height} [By ${userId}]`);
+        this.addLog(`[Zoom] x: ${x}, y: ${y}, width: ${width}, height: ${height} [By ${userId}]`);
     }
 }
 export default Logger;

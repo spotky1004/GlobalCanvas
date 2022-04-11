afbd8831a771810d2a5e449d390559cf185e2c88
Spotky1004 <spotky1004@gmail.com>
2022-04-12 01:35:08 +0900

    Fix cooldown format

--- src/util/discordCooldownFormat.ts
+++ src/util/discordCooldownFormat.ts
@@ -1,4 +1,4 @@
 export default function discordCooldownFormat(currentTime: number, leftTime: number) {
   const timestamp = Math.floor((currentTime + leftTime)/1000);
-  return `<t:${timestamp}:f> (${(leftTime/100).toFixed(2)}s left)`;
+  return `<t:${timestamp}:f> (**${(leftTime/1000).toFixed(2)}s** left)`;
 }

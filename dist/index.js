97c7f2dd649ae4f4dc1933c73521cc218faf8436
Spotky1004 <spotky1004@gmail.com>
2022-04-12 23:03:26 +0900

    Maintenance

--- dist/index.js
+++ dist/index.js
@@ -76,22 +76,24 @@ client.on("messageCreate", async (message) => {
     }
 });
 client.on("interactionCreate", async (interaction) => {
-    try {
-        if (interaction.isCommand()) {
-            const isCommandVaild = handlers.command(app, interaction);
-            if (!isCommandVaild) {
-                await interaction.reply({ content: "Invaild command", ephemeral: true });
-            }
-        }
-    }
-    catch (e) {
-        console.log(e);
-    }
+    interaction;
+    // try {
+    //   if (interaction.isCommand()) {
+    //     const isCommandVaild = handlers.command(app, interaction);
+    //     if (!isCommandVaild) {
+    //       await interaction.reply({ content: "Invaild command", ephemeral: true });
+    //     }
+    //   }
+    // }  catch (e) { console.log(e); }
 });
 setInterval(() => {
     app.save();
     if (client.user) {
-        client.user.setActivity(getRandomTrivia({ app, client }), {
+        getRandomTrivia;
+        // client.user.setActivity(getRandomTrivia({ app, client }), {
+        //   type: "WATCHING"
+        // });
+        client.user.setActivity("Maintenance for 10~30 hours", {
             type: "WATCHING"
         });
     }
